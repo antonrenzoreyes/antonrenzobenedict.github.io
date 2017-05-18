bfeb81da4ec938275df83a5a188b5b39c0fc944b
Anton-Renzo Benedict Reyes <antonrenzobenedict@gmail.com>
2017-05-18 15:49:36 +0800

    Update

--- js/learnKanji.js
+++ js/learnKanji.js
@@ -1,88 +1,167 @@
 var kanjiData =
 [
     {
-        'image' : 'images/kanji/fun.gif',
-        'onyomi' : 'FUN',
-        'kunyomi': 'wa(karu)',
-        'meaning': 'understand',
-        'mnemonic': '',
+        "image" : "images/kanji/fun.gif",
+        "onyomi" : "FUN",
+        "kunyomi": "wa(karu)",
+        "meaning": "understand",
+        "mnemonic": "",
 
     },
     {
-        'image' : 'images/kanji/bun.gif',
-        'onyomi' : 'BUN, MON',
-        'kunyomi': 'ki(ku), ki(koeru)',
-        'meaning': 'to hear, to listen, to ask',
-        'mnemonic': '',
+        "image" : "images/kanji/bun.gif",
+        "onyomi" : "BUN, MON",
+        "kunyomi": "ki(ku), ki(koeru)",
+        "meaning": "to hear, to listen, to ask",
+        "mnemonic": "",
 
     },
     {
-        'image' : 'images/kanji/bo.gif',
-        'onyomi' : 'BO',
-        'kunyomi': 'haha',
-        'meaning': 'mother',
-        'mnemonic': '',
+        "image" : "images/kanji/bo.gif",
+        "onyomi" : "BO",
+        "kunyomi": "haha",
+        "meaning": "mother",
+        "mnemonic": "",
 
     },
     {
-        'image' : 'images/kanji/hoku.gif',
-        'onyomi' : 'HOKU',
-        'kunyomi': 'kita',
-        'meaning': 'north',
-        'mnemonic': '',
+        "image" : "images/kanji/hoku.gif",
+        "onyomi" : "HOKU",
+        "kunyomi": "kita",
+        "meaning": "north",
+        "mnemonic": "",
 
     },
     {
-        'image' : 'images/kanji/boku.gif',
-        'onyomi' : 'BOKU, MOKU',
-        'kunyomi': 'ki, ko',
-        'meaning': 'tree, wood',
-        'mnemonic': '',
+        "image" : "images/kanji/boku.gif",
+        "onyomi" : "BOKU, MOKU",
+        "kunyomi": "ki, ko",
+        "meaning": "tree, wood",
+        "mnemonic": "",
 
     },
     {
-        'image' : 'images/kanji/hon.gif',
-        'onyomi' : 'HON',
-        'kunyomi': 'moto',
-        'meaning': 'book, source, main-',
-        'mnemonic': '',
+        "image" : "images/kanji/hon.gif",
+        "onyomi" : "HON",
+        "kunyomi": "moto",
+        "meaning": "book, source, main-",
+        "mnemonic": "",
 
     },
     {
-        'image' : 'images/kanji/mai.gif',
-        'onyomi' : 'MAI',
-        'kunyomi': '-',
-        'meaning': 'each, every',
-        'mnemonic': '',
+        "image" : "images/kanji/mai.gif",
+        "onyomi" : "MAI",
+        "kunyomi": "-",
+        "meaning": "each, every",
+        "mnemonic": "",
 
     },
     {
-        'image' : 'images/kanji/man.gif',
-        'onyomi' : 'MAN, BAN',
-        'kunyomi': '-',
-        'meaning': 'ten thousand, all, many',
-        'mnemonic': '',
+        "image" : "images/kanji/man.gif",
+        "onyomi" : "MAN, BAN",
+        "kunyomi": "-",
+        "meaning": "ten thousand, all, many",
+        "mnemonic": "",
 
     },
     {
-        'image' : 'images/kanji/mei.gif',
-        'onyomi' : 'MEI, MYOU',
-        'kunyomi': 'na',
-        'meaning': 'name, reputation',
-        'mnemonic': '',
+        "image" : "images/kanji/mei.gif",
+        "onyomi" : "MEI, MYOU",
+        "kunyomi": "na",
+        "meaning": "name, reputation",
+        "mnemonic": "",
 
     },
     {
-        'image' : 'images/kanji/moku.gif',
-        'onyomi' : 'MOKU',
-        'kunyomi': 'me',
-        'meaning': 'eye',
-        'mnemonic': '',
+        "image" : "images/kanji/moku.gif",
+        "onyomi" : "MOKU",
+        "kunyomi": "me",
+        "meaning": "eye",
+        "mnemonic": "",
+
+    },
+    {
+        "image" : "images/kanji/do.gif",
+        "onyomi" : "DO, TO",
+        "kunyomi": "Tsuchi",
+        "meaning": "Earth, ground",
+        "mnemonic": "Cross on the ground",
+
+    },
+    {
+        "image" : "images/kanji/jin.gif",
+        "onyomi" : "JIN, NIN",
+        "kunyomi": "Hito",
+        "meaning": "Person",
+        "mnemonic": "Like a stick person's legs",
+
+    },
+    {
+        "image" : "images/kanji/kyuu.gif",
+        "onyomi" : "KYUU",
+        "kunyomi": "Yasu(mu)",
+        "meaning": "To Rest",
+        "mnemonic": "Person resting on a tree",
+
+    },
+    {
+        "image" : "images/kanji/u.gif",
+        "onyomi" : "U",
+        "kunyomi": "Ame",
+        "meaning": "Rain",
+        "mnemonic": "Closed windows because of the rain",
+
+    },
+    {
+        "image" : "images/kanji/zen.gif",
+        "onyomi" : "ZEN",
+        "kunyomi": "Mae",
+        "meaning": "Before",
+        "mnemonic": "Thick TV with antenna was used before",
+
+    },
+    {
+        "image" : "images/kanji/gaku.gif",
+        "onyomi" : "GAKU",
+        "kunyomi": "Mana(bu)",
+        "meaning": "School, science, teaching",
+        "mnemonic": "Child gaining knowledge",
+
+    },
+    {
+        "image" : "images/kanji/ge.gif",
+        "onyomi" : "KA, GE",
+        "kunyomi": "Shimo, sa(geru), o(rosu), ku(daru)",
+        "meaning": "Below, down",
+        "mnemonic": "Sign pointing down",
+
+    },
+    {
+        "image" : "images/kanji/ta.gif",
+        "onyomi" : "TA",
+        "kunyomi": "Oo(i)",
+        "meaning": "Many",
+        "mnemonic": "Many TAwers (towers)",
+
+    },
+    {
+        "image" : "images/kanji/haku.gif",
+        "onyomi" : "HAKU, BYAKU",
+        "kunyomi": "Shiro(i), shiro",
+        "meaning": "White",
+        "mnemonic": "White Candle",
+
+    },
+    {
+        "image" : "images/kanji/jo.gif",
+        "onyomi" : "JO, NYO",
+        "kunyomi": "Onna, me",
+        "meaning": "Woman, female",
+        "mnemonic": "Woman sitting near a table",
 
     },
 
 ];
-
 for(var i = 0; i<kanjiData.length; i++){
 
 	if(i % 4 == 0)
