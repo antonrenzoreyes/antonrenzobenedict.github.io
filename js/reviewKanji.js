934cbb133bad74d57512b4d6818642789a70a9be
Anton-Renzo Benedict Reyes <antonrenzobenedict@gmail.com>
2017-05-18 21:50:20 +0800

    add randomizzation

--- js/reviewKanji.js
+++ js/reviewKanji.js
@@ -322,6 +322,8 @@ var kanjiData =
     },
 
 ];
+
+kanjiData = shuffleArray(kanjiData)
 for(var i = 0; i<kanjiData.length; i++){
 
 	if(i % 4 == 0)
@@ -333,5 +335,15 @@ for(var i = 0; i<kanjiData.length; i++){
     {
     	$('#cards').append("</div>")
     }
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
 }
 
